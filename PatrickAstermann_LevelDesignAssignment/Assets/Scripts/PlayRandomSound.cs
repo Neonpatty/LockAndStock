57ddc87d234f15311aaf8a653cc47770d2a692a1
Patrick <patrick.astermann01@gmail.com>
2021-05-04 16:45:45 +1000

    Alot was done
    -Textured the rest of models
    -played with lighting and fog
    -made silhouette neighbourhood
    -Played with sound effects
    -did one more pass on my feature asset

---
+++ PatrickAstermann_LevelDesignAssignment/Assets/Scripts/PlayRandomSound.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class PlayRandomSound : MonoBehaviour
+{
+    public AudioSource _as;
+
+    public AudioClip[] AudioClipArray;
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        _as = GetComponent<AudioSource>();
+        _as.clip = AudioClipArray[Random.Range(0, AudioClipArray.Length)];
+        _as.PlayOneShot(_as.clip);
+    }
+
+    void Update()
+    {
+        
+    }
+}
